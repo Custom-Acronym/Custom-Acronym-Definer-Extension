36832e0c2f20b162bdf00a941f0d031a5f941592
Robert Beckwith <robertb4@illinois.edu>
2020-11-10 23:45:03 -0600

    bootstrap format search

--- CustomAcronyms/search.js
+++ CustomAcronyms/search.js
@@ -1,14 +1,10 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
 'use strict';
 
-function submitSearch(event) {
-  log.textContent = `Form Submitted! Time stamp: ${event.timeStamp}`;
-  console.log(event);
-  event.preventDefault();
-}
-const form = document.getElementById('search')
+const getAcronymForm = document.getElementById("getAcronym");
 
-form.addEventListener('submit', submitSearch)
+
+addAcronymForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  addAcronym(document.getElementById("addAcronymInput"));
+});
