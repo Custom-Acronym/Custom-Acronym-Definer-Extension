31c389f3492a2eff50a467045e8b28a6788e0a12
Robert Beckwith <robertb4@illinois.edu>
2020-11-11 00:42:54 -0600

    move api requests to new file

---
+++ CustomAcronyms/apiRequests.js
@@ -0,0 +1,58 @@
+/* 
+* get Acronym from server.
+* on successful get request handle displaying the acronym.
+*/
+function getAcronym(handleDisplay, acronym) {
+
+    const XHR = new XMLHttpRequest();
+
+    XHR.addEventListener("load", handleDisplay);
+
+    XHR.addEventListener("error", function (event) {
+        alert('Acronym not found!');
+    });
+
+    XHR.open("GET", GET_ACRONYM_URL + acronym);
+    XHR.send()
+}
+
+/*
+* Add acronym to the database 
+*/
+function addAcronym(form) {
+    console.log("Sending Data!")
+    const XHR = new XMLHttpRequest();
+
+    const FD = new FormData(form);
+
+    // Define what happens on successful data submission
+    XHR.addEventListener("load", function (event) {
+        alert(event.target.responseText);
+    });
+
+    // Define what happens in case of error
+    XHR.addEventListener("error", function (event) {
+        alert('Oops! Something went wrong.');
+    });
+
+    XHR.open("POST", "https://example.com/cors.php");
+    XHR.send(FD);
+}
+/*
+* Update acronym to the database 
+*/
+function updateAcronym(input) {
+    const XHR = new XMLHttpRequest();
+
+    XHR.addEventListener("load", function (event) {
+      alert(event.target.responseText);
+    });
+
+    XHR.addEventListener("error", function (event) {
+      alert('Acronym not found!');
+    });
+
+    XHR.open('PUT', PUT_ACRONYM_URL);
+    console.log(input.value);
+    XHR.send(input.value);
+  }
