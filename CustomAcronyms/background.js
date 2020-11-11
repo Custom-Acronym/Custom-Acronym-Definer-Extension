f2893ad3790c3cbadd5a52a994e215a642fb1492
Robert Beckwith <robertb4@illinois.edu>
2020-11-11 15:55:42 -0600

    add defition request for content script

--- CustomAcronyms/background.js
+++ CustomAcronyms/background.js
@@ -1,19 +1,37 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 'use strict';
 
 // Listen for key combinations, TODO: Handle these
-chrome.commands.onCommand.addListener(function(command) {
+chrome.commands.onCommand.addListener(function (command) {
   console.log('Command:', command);
 });
 
+function sendDefition(event){
+  let definition = "";
+  if(event.target.responseText == '[]'){
+    definition = "Acronym not defined"
+  }
+  else{
+    definition = JSON.parse(event.target.responseText)[0].definition
+  }
+  chrome.tabs.sendMessage(sender.tab.id, { definition: definition});
+}
+
 // Listen for a message from the 'More' button
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    chrome.windows.create({
-      url: chrome.runtime.getURL('addAcronym.html'),
-      type: 'popup'
-    })
+  function (request, sender, sendResponse) {
+    if (request.acronym) {
+      const XHR = new XMLHttpRequest();
+      XHR.addEventListener("load", sendDefition);
+      XHR.open("GET", GET_ACRONYM_URL + request.acronym);
+      XHR.send();
+    }
+    else if (request.button) {
+      chrome.windows.create({
+        url: chrome.runtime.getURL('addAcronym.html'),
+        type: 'popup',
+        height: 680,
+        width: 470
+      })
+    }
+
   });
